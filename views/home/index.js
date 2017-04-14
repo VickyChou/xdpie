6d6222c3cd14dc136e06b621e5e2709fe86a36b6
vicky <vickyyoumay@hotmail.com>
2017-04-14 18:20:14 +0800

    init

--- views/home/index.js
+++ views/home/index.js
@@ -1,8 +1,7 @@
-angular.module('xdpie', ['ui.router','ngMaterial','ui.bootstrap'])
+angular.module('xdpie', ['ui.router','ngMaterial'])
     .config(function ($stateProvider,$urlRouterProvider) {
         $urlRouterProvider.when('','/');
         $stateProvider
-
             .state('questionary', {
                 url: '/questionary',
                 templateUrl: '/views/home/questionary/questionary.html'
